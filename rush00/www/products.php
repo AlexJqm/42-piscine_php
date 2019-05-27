0f185593291c6f5a61189ae8d5eb4d08edd75c13
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2019-05-27 11:37:24 +0200

    27-05-2019

--- rush00/www/products.php
+++ rush00/www/products.php
@@ -31,7 +31,7 @@
 	global $con;
 	$get_cats = "select * from categories";
 	$run_cats = mysqli_query($con, $get_cats);
-	while ($row_cats=mysqli_fetch_array($run_cats)) {
+	while ($row_cats = mysqli_fetch_array($run_cats)) {
 		$cat_id = $row_cats['cat_id'];
 		$cat_title = $row_cats['cat_title'];
 		echo "
