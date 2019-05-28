178ac0588bd44a356b2fdbccf97bc9acc7be6351
Alexandre Jacquemin <39719307+AlexJqm@users.noreply.github.com>
2019-05-28 16:14:38 +0200

    Update ex17.sql

--- day05/ex17/ex17.sql
+++ day05/ex17/ex17.sql
@@ -1,2 +1,2 @@
-SELECT COUNT(id_abo) AS `nb_susc`, FLOOR(SUM(price) / COUNT(price)) AS `av_susc`, (SUM(duration_sub) % 42) AS `ft`
+SELECT COUNT(id_sub) AS `nb_susc`, FLOOR(SUM(price) / COUNT(price)) AS `av_susc`, (SUM(duration_sub) % 42) AS `ft`
 FROM subscription;
